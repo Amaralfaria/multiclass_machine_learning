5a96448b9c013565931725ee7eb237077a39b491
Lucas Amaral <lucasamaralfaria@gmail.com>
2023-12-03 19:40:13 -0300

    mudancas para notebook

--- src/OvREST.py
+++ src/OvREST.py
@@ -1,47 +1,93 @@
 import pandas as pd
 import numpy as np
 from sklearn.linear_model import LogisticRegression
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, accuracy_score
 
-data_path = 'C:/Users/ionaa/OneDrive/Documentos/unb/quinto_semestre/iia/seminiario/data/Dry_Bean_Dataset.xlsx'
+
+data_path = 'data/Dry_Bean_Dataset.xlsx'
 
 
 beans_data = pd.read_excel(data_path)
 X = beans_data.iloc[:,1:-1]
 y = beans_data.Class
 
-# Assuming X_train, y_train are your training data and labels
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=16)
 
 
-models = []
+# Minha implementação
+# models = []
 
-for label in np.unique(y):
-    filter = lambda r:(1 if r == label else -1)
-    y_train_class = np.array([filter(row) for row in y_train])
-    y_test_class = np.array([filter(row) for row in y_test])
+# for label in np.unique(y):
+#     filter = lambda r:(1 if r == label else -1)
+#     y_train_class = np.array([filter(row) for row in y_train])
+#     y_test_class = np.array([filter(row) for row in y_test])
 
-    model = LogisticRegression(max_iter=300, random_state=16)
-    model.fit(X_train,y_train_class)
-    models.append((model,label))
-
-
-y_pred = np.empty(len(y_test), dtype='O')
-max_prob = np.zeros(len(y_train))
-
-for model in models:
-    probability = model[0].predict_proba(X_test)[:,1]
-    for i,prob in enumerate(probability):
-        if max_prob[i] < prob:
-            max_prob[i] = prob
-            y_pred[i] = model[1]
+#     model = DecisionTreeClassifier()
+#     model.fit(X_train,y_train_class)
+#     models.append((model,label))
 
 
+# y_pred = np.empty(len(y_test), dtype='O')
+# max_prob = np.zeros(len(y_train))
 
-print(classification_report(y_test, y_pred))
-print('Confusion matrix:')
-print(confusion_matrix(y_test, y_pred))
+# for model in models:
+#     probability = model[0].predict_proba(X_test)[:,1]
+#     for i,prob in enumerate(probability):
+#         if max_prob[i] <= prob:
+#             max_prob[i] = prob
+#             y_pred[i] = model[1]
+
+
+#Usando biblioteca
+
+# model = DecisionTreeClassifier()
+
+# ovr = OneVsRestClassifier(model)
+
+# ovr.fit(X_train,y_train)
+
+# y_pred = ovr.predict(X_test)
+# print(np.unique(y_pred))
+
+
+# print(classification_report(y_test, y_pred))
+# print('Confusion matrix:')
+# print(confusion_matrix(y_test, y_pred))
+
+modelos = [
+    ('Decision Tree', DecisionTreeClassifier()),
+    ('KNN', KNeighborsClassifier(n_neighbors=5)),
+    ('Logistic Regression', LogisticRegression()),
+    ('Naive Bayes', GaussianNB())
+
+    # Adicione outros modelos conforme necessário
+]
+
+# Lista para armazenar as acurácias de cada modelo
+acuracias = []
+
+# Treine e avalie cada modelo
+for nome, modelo in modelos:
+    ovr = OneVsRestClassifier(modelo)
+    ovr.fit(X_train, y_train)
+    y_pred = ovr.predict(X_test)
+    acuracia = accuracy_score(y_test, y_pred)
+    acuracias.append((nome, acuracia))
+
+# Crie um gráfico de barras para comparar as acurácias
+nomes_modelos, valores_acuracia = zip(*acuracias)
+plt.bar(nomes_modelos, valores_acuracia, color=['blue', 'green', 'red', 'black'])  # Adicione mais cores conforme necessário
+plt.ylabel('Acurácia')
+plt.title('Comparação de Acurácia entre Modelos')
+plt.ylim(0, 1)  # Defina o limite y de 0 a 1 para a acurácia
+plt.show()
 
 
--- src/adag.py
+++ src/adag.py
@@ -1,9 +1,12 @@
 import pandas as pd
 import numpy as np
 from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
+from sklearn.multiclass import OneVsOneClassifier
 import copy
 
 def initialLevel(n_labels):
@@ -32,64 +35,69 @@ y = beans_data.Class
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=16)
 
-class_array = np.unique(y)
-X_train_array = np.array(X_train)
-y_train_array = np.array(y_train)
-X_test_array = np.array(X_test)
-lables_data = []
-models = np.empty((len(class_array),len(class_array)), dtype='O')
-y_pred = np.empty(len(y_test), dtype='O')
+# scaler = StandardScaler()
+# X_train = scaler.fit_transform(X_train)
+# X_test = scaler.transform(X_test)
+
+# class_array = np.unique(y)
+# X_train_array = np.array(X_train)
+# y_train_array = np.array(y_train)
+# X_test_array = np.array(X_test)
+# lables_data = []
+# models = np.empty((len(class_array),len(class_array)), dtype='O')
+# y_pred = np.empty(len(y_test), dtype='O')
 
 
 
 
-for i,label in enumerate(class_array):
-    data = np.array([row for index,row in enumerate(X_train_array) if y_train_array[index] == label])
-    lables_data.append(data)
+# for i,label in enumerate(class_array):
+#     data = np.array([row for index,row in enumerate(X_train_array) if y_train_array[index] == label])
+#     lables_data.append(data)
 
 
-for i in range(len(class_array)):
-    for j in range(i+1,len(class_array)):
-        x_train_class = []
-        for row in lables_data[i]:
-            x_train_class.append(row)
-        for row in lables_data[j]:
-            x_train_class.append(row)
+# for i in range(len(class_array)):
+#     for j in range(i+1,len(class_array)):
+#         x_train_class = []
+#         for row in lables_data[i]:
+#             x_train_class.append(row)
+#         for row in lables_data[j]:
+#             x_train_class.append(row)
 
 
-        y_train_class = [1]*len(lables_data[i]) + [0]*len(lables_data[j])
-        model = LogisticRegression(max_iter=300,random_state=16)
-        model.fit(np.array(x_train_class),np.array(y_train_class))
-        models[i][j] = model
-        models[j][i] = -1
-
-
-
-for i,row in enumerate(X_test_array):
-    levelLabels = initialLevel(len(class_array))
-
-    while len(levelLabels) != 1:
-        next_level = []
-
-        for idx in range(0,len(levelLabels)-1 - (len(levelLabels)%2),2):
-            first = min(levelLabels[idx],levelLabels[idx+1])
-            second = max(levelLabels[idx],levelLabels[idx+1])
-
-            predicao = models[first][second].predict([row])
-            if predicao[0] == 1:
-                next_level.append(first)
-            else:
-                next_level.append(second)
-
-        if len(levelLabels)%2 > 0:
-            next_level.append(levelLabels[len(levelLabels)-1])
+#         y_train_class = [1]*len(lables_data[i]) + [0]*len(lables_data[j])
+#         model = LogisticRegression(max_iter=300,random_state=16)
+#         # model = KNeighborsClassifier(n_neighbors=5)
+#         model.fit(np.array(x_train_class),np.array(y_train_class))
+#         models[i][j] = model
+#         models[j][i] = -1
 
 
 
-        levelLabels = copy.deepcopy(next_level)
+# for i,row in enumerate(X_test_array):
+#     levelLabels = initialLevel(len(class_array))
+
+#     while len(levelLabels) != 1:
+#         next_level = []
+
+#         for idx in range(0,len(levelLabels)-1 - (len(levelLabels)%2),2):
+#             first = min(levelLabels[idx],levelLabels[idx+1])
+#             second = max(levelLabels[idx],levelLabels[idx+1])
+
+#             predicao = models[first][second].predict([row])
+#             if predicao[0] == 1:
+#                 next_level.append(first)
+#             else:
+#                 next_level.append(second)
+
+#         if len(levelLabels)%2 > 0:
+#             next_level.append(levelLabels[len(levelLabels)-1])
+
+
+
+#         levelLabels = copy.deepcopy(next_level)
     
 
-    y_pred[i] = class_array[levelLabels[0]]
+#     y_pred[i] = class_array[levelLabels[0]]
 
 # for i,row in enumerate(X_test_array):
 #     indexes = list(range(len(class_array)))
@@ -119,7 +127,9 @@ for i,row in enumerate(X_test_array):
 #     y_pred[i] = class_array[indexes[0]]
 
 
-
+ovo = OneVsOneClassifier(LogisticRegression(max_iter=300))
+ovo.fit(X_train,y_train)
+y_pred = ovo.predict(X_test)
 
 
 
